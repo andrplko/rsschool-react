2dbb87294bcc70466ef3a2ff6793337815717efc
Andrei Paleshka <andrepoleshko@gmail.com>
2023-11-19 21:28:14 +0100

    fix: remove local storage constants

--- src/constants/index.ts
+++ src/constants/index.ts
@@ -1,6 +1,3 @@
-export const LOCAL_STORAGE_SEARCH_TERM = 'searchTerm';
-export const LOCAL_STORAGE_IS_OPEN = 'isOpen';
-
 export const DEFAULT_PER_PAGE = 6;
 export const DEFAULT_CURRENT_PAGE = 1;
 export const DEFAULT_TOTAL_PAGES = 1;
