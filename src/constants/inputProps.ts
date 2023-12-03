ad28c88045b922d65e6ef369ec26fe6d04756b3a
Andrei Paleshka <andrepoleshko@gmail.com>
2023-12-03 13:08:09 +0100

    feat: implement constants

---
+++ src/constants/inputProps.ts
@@ -0,0 +1,44 @@
+export const inputProps = [
+  {
+    id: 'name',
+    name: 'name',
+    label: 'Name',
+    type: 'text',
+    placeholder: 'Name',
+  },
+  {
+    id: 'age',
+    name: 'age',
+    label: 'Age',
+    type: 'number',
+    placeholder: 'Age',
+  },
+  {
+    id: 'email',
+    name: 'email',
+    label: 'Email',
+    type: 'email',
+    placeholder: 'Email',
+  },
+  {
+    id: 'password',
+    name: 'password',
+    label: 'Password',
+    type: 'password',
+    placeholder: 'Password',
+  },
+  {
+    id: 'confirm_password',
+    name: 'confirm_password',
+    label: 'Confirm password',
+    type: 'password',
+    placeholder: 'Confirm password',
+  },
+  {
+    id: 'picture',
+    name: 'picture',
+    label: 'Choose a picture',
+    type: 'file',
+    accept: '.jpg, .jpeg, .png',
+  },
+];
