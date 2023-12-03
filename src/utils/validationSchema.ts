6fa0c2ccda38911d776df6494fce34b2c6db97b8
Andrei Paleshka <andrepoleshko@gmail.com>
2023-12-03 23:51:00 +0100

    feat: update utils

--- src/utils/validationSchema.ts
+++ src/utils/validationSchema.ts
@@ -1,5 +1,8 @@
 import { z } from 'zod';
 
+const MAX_FILE_SIZE = 1024 * 1024 * 3;
+const ACCEPTED_IMAGE_TYPES = ['image/jpeg', 'image/jpg', 'image/png'];
+
 const validationSchema = z
   .object({
     name: z
@@ -45,11 +48,40 @@ const validationSchema = z
     confirm_password: z
       .string({ required_error: 'Confirm password is required' })
       .min(1, { message: 'Confirm password is required' }),
-    accept_terms: z
-      .boolean({ required_error: 'Accept terms is required' })
-      .refine((value) => value === true, {
-        message: 'You must accept terms and conditions',
-      }),
+    gender: z
+      .string({ required_error: 'Gender is required' })
+      .min(1, { message: 'Please select a gender' }),
+    accept_terms: z.union([
+      z
+        .boolean({ required_error: 'Accept terms is required' })
+        .refine((value) => value, {
+          message: 'You must accept terms and conditions',
+        }),
+      z
+        .string({ required_error: 'Accept terms is required' })
+        .refine((value) => value === 'on', {
+          message: 'You must accept terms and conditions',
+        }),
+    ]),
+    picture: z.union([
+      z
+        .any()
+        .refine((file) => file?.size <= MAX_FILE_SIZE, `Max image size is 3MB.`)
+        .refine(
+          (file) => ACCEPTED_IMAGE_TYPES.includes(file?.type),
+          'Only .jpg, .jpeg, .png formats are supported.'
+        ),
+      z
+        .any()
+        .refine(
+          (files) => files?.[0]?.size <= MAX_FILE_SIZE,
+          `Max image size is 3MB.`
+        )
+        .refine(
+          (files) => ACCEPTED_IMAGE_TYPES.includes(files?.[0]?.type),
+          'Only .jpg, .jpeg, .png formats are supported.'
+        ),
+    ]),
   })
   .refine((data) => data.password === data.confirm_password, {
     path: ['confirm_password'],
